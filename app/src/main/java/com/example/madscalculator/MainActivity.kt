f430b37ed458e57a363fe7058e350d7c274b8d3c
Ashish Gupta <gupta.ashish@lattukids.com>
2020-09-03 17:07:24 +0530

    added calculator layout

---
+++ app/src/main/java/com/example/madscalculator/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.madscalculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var intent = Intent(this,CalculatorActivity::class.java)
+        startActivity(intent)
+    }
+}
