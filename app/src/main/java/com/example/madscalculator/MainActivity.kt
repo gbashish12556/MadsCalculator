4634671df39c379157c370f32d066bcfc65ed008
Ashish Gupta <gupta.ashish@lattukids.com>
2020-09-04 10:49:30 +0530

    added login

--- app/src/main/java/com/example/madscalculator/MainActivity.kt
+++ app/src/main/java/com/example/madscalculator/MainActivity.kt
@@ -1,15 +1,42 @@
 package com.example.madscalculator
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
+
+    var userNameEditText:EditText? = null
+    var userPassEditText:EditText? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        var intent = Intent(this,CalculatorActivity::class.java)
-        startActivity(intent)
+
+
+        userNameEditText = findViewById(R.id.userName)
+        userPassEditText = findViewById(R.id.userPassword)
+
+        findViewById<Button>(R.id.loginButton).setOnClickListener{
+
+            var userName = userNameEditText?.text.toString()
+            var userPassword = userPassEditText?.text.toString()
+
+            if(userName.equals("Ashish") && userPassword.equals("123456$")){
+                var intent = Intent(this,CalculatorActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this,"Invalid User Or Password",Toast.LENGTH_LONG).show();
+            }
+        }
     }
+
+
+
+
 }
